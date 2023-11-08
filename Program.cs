3a8ffc2513e14116d6b6ba9c00a0ad0227b04cfe
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2023-11-08 13:12:01 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace MySite
+{
+  public class Program
+  {
+    public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
+  }
+}
